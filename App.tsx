e2b2743621612935a5505aee559670b7363dd168
Hanqing Wu <92377340+GotYoo@users.noreply.github.com>
2025-12-25 17:37:57 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
   const [userImages, setUserImages] = useState<string[]>([
     'photos/1.jpg',
     'photos/2.jpg',
-    'photos/3.png',
+    'photos/3.jpg',
     'photos/4.jpg',
     'photos/5.jpg',
     'photos/6.jpg',
