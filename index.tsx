c8ddd002d33c6d0a4b823ec5232c60682d7bdf60
Hanqing Wu <92377340+GotYoo@users.noreply.github.com>
2025-12-25 16:00:39 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// 👇 必须加上这一行！没有它，整个网站的布局都会失效（黑屏/白屏）
+import './index.css'; 
 import App from './App';
 
 const rootElement = document.getElementById('root');
